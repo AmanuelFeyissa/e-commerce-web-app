6c5fff8191616b7d718e351b0ff7730c5ba8fe12
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-01-24 22:19:58 +0300

    Created some new helper functions
    Created some new helper functions for
    - redirecting
    - confirming
    - mysqli_query
    - escape_string
    _ fetch mysqli_array

--- resources/functions.php
+++ resources/functions.php
@@ -1,3 +1,36 @@
 <?php
 
+// a function to redirect
+function redirect($location){
+
+    header("Location: $location ");
+}
+
+function query($sql){
+
+    global $connection;
+    return mysqli_query($connection, $sql);
+}
+
+function confirm($result) {
+    global $connection;
+
+    if (!$result){
+        die("QUERY FAILED " . mysqli_error($connection));
+    }
+}
+
+// to prevent sql injection attacks and to ignore spaces and other things when we using login
+function escape_string($string) {
+
+    global $connection;
+
+    return mysqli_real_escape_string($connection, $string);
+}
+
+function fetch_array($result) {
+
+    return mysqli_fetch_array($result);
+}
+
 ?>
