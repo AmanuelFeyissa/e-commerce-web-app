285443e482c912b521141a609bc13dae5e8856a0
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-01-24 17:54:00 +0300

    Defining path and constants
    created a path for our two folders (front and back)

--- resources/config.php
+++ resources/config.php
@@ -2,9 +2,9 @@
 
 defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
 
-echo __DIR__;
+defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "template/front");
 
-
+defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "template/back");
 
 
 
