95e5cea54d93d6e68a5284aefed7215a8a643ed0
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-01-26 22:59:07 +0300

    updated the get product function
    added a delimeter to display multiple strings at once and added the products template inside it

--- resources/functions.php
+++ resources/functions.php
@@ -44,7 +44,24 @@ function get_products() {
 
     while($row = fetch_array($query)) {
 
-        echo $row['product_price'];
+        $product = <<< DELIMETER
+        <div class="col-sm-4 col-lg-4 col-md-4">
+        <div class="thumbnail">
+            <img src="http://placehold.it/320x150" alt="">
+            <div class="caption">
+                <h4 class="pull-right">Birr {$row['product_price']}</h4>
+                <h4><a href="product.html">First Product</a>
+                </h4>
+                <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+                <a class="btn btn-primary" target="_blank" href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">View Tutorial</a>
+            </div>
+            
+        </div>
+        </div>
+
+        DELIMETER;
+
+        echo $product;
     }
 }
 
