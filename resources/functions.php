be3f1a23b9d43eec870e1b07c8afc96c57f7fe52
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-01-26 23:21:02 +0300

    Update functions.php
    Changed the image href and add to cart button to redirect to item.php with its own product id from db

--- resources/functions.php
+++ resources/functions.php
@@ -47,13 +47,13 @@ function get_products() {
         $product = <<< DELIMETER
         <div class="col-sm-4 col-lg-4 col-md-4">
         <div class="thumbnail">
-            <img src="http://placehold.it/320x150" alt="">
+            <a href = "item.php?id={$row['product_id']}" > <img src="{$row['product_image']}" alt=""></a>
             <div class="caption">
                 <h4 class="pull-right">Birr {$row['product_price']}</h4>
-                <h4><a href="product.html">First Product</a>
+                <h4><a href="product.html">{$row['product_title']}</a>
                 </h4>
                 <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
-                <a class="btn btn-primary" target="_blank" href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">View Tutorial</a>
+                <a class="btn btn-primary" target="_blank" href="item.php?id={$row['product_id']}">Add to cart</a>
             </div>
             
         </div>
