7d9470592ce487a3fe039716a6e2b533ab7b8567
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-01-28 22:44:18 +0300

    replaced the category code snippet with get_categories function
    replaced the categories snippet with the new function get categories which holds all the snippets

--- resources/templates/front/side_nav.php
+++ resources/templates/front/side_nav.php
@@ -4,19 +4,7 @@
 
         <?php
 
-            $query = "SELECT * FROM categories";
-            $send_query = mysqli_query($connection, $query);
-
-            // to check if the query failed
-            if(!$send_query) {
-
-                die("QUERY FAILED" . mysqli_error($connection));
-            }
-
-            while($row = mysqli_fetch_array($send_query)){
-
-                echo "<a href='#' class='list-group-item'>{$row['cat_title']}</a>";
-            }
+            get_categories();
 
         ?>
 
