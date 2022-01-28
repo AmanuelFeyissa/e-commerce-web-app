a5bc6a802e71756744a84327670182f26af9ed6d
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-01-29 01:15:14 +0300

    added a display message
    added a display message that displays a message if the user types a wrong password or username

--- public/login.php
+++ public/login.php
@@ -7,6 +7,7 @@
 
       <header>
             <h1 class="text-center">Login</h1>
+            <h2 class="text-center bg-warning"><?php display_message(); ?></h2>
         <div class="col-sm-4 col-sm-offset-5">         
             <form class="" action="" method="post" enctype="multipart/form-data">
 
