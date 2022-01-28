327a83de3e384ee57775cb53e9ccf7adb78a1974
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-01-29 00:06:46 +0300

    created get products in categoty page function
    added a herodoc called delimeter to display multiple strings at once and added the category.php template inside it

--- resources/functions.php
+++ resources/functions.php
@@ -92,6 +92,33 @@ function get_categories() {
 }
 
 
+function get_products_in_cat_page() {
+
+    $query = query(" SELECT * FROM products WHERE product_category_id =" . escape_string($_GET['id']). " ");
+    confirm($query);
+
+    while($row = fetch_array($query)) {
+        // Creating a herodoc
+        $product = <<< DELIMETER
+        <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="{$row['product_image']}" alt="">
+                    <div class="caption">
+                        <h3>{$row['product_title']}</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="item.php?id={$row['product_id']}" class="btn btn-default">More Info</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        DELIMETER;
+
+        echo $product;
+    }
+}
+
 
 
 
