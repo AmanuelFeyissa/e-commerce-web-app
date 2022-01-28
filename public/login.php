7f6e45c61d79f5e2ba579fd331db6d99d60df6ef
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-01-29 00:56:55 +0300

    linked the login user function to login.php

--- public/login.php
+++ public/login.php
@@ -9,6 +9,9 @@
             <h1 class="text-center">Login</h1>
         <div class="col-sm-4 col-sm-offset-5">         
             <form class="" action="" method="post" enctype="multipart/form-data">
+
+                <?php login_user(); ?>
+
                 <div class="form-group"><label for="">
                     username<input type="text" name="username" class="form-control"></label>
                 </div>
