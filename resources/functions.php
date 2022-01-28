23e28eb38a8dac2e2364a3176ada2b0720fa36e8
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-01-28 22:42:25 +0300

    created a get categories function
    added a herodoc called delimeter to display multiple strings at once and added the categories template inside it

--- resources/functions.php
+++ resources/functions.php
@@ -65,4 +65,24 @@ function get_products() {
     }
 }
 
+function get_categories() {
+
+    
+    $query = query("SELECT * FROM categories");
+
+    confirm($query);
+
+    while($row = mysqli_fetch_array($query)){
+        
+        $category_links = <<< DELIMETER
+
+         <a href='category.php?id={$row['cat_id']}' class='list-group-item'>{$row['cat_title']}</a>
+        
+        DELIMETER;
+
+        echo $category_links;
+
+    }
+}
+
 ?>
