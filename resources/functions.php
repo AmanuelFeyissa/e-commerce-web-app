dc0728f5ee1132af6693ac9d361679b7abb6176f
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-01-31 17:31:52 +0300

    Update functions.php

--- resources/functions.php
+++ resources/functions.php
@@ -76,7 +76,7 @@ function get_products() {
                 <h4><a href="item.php?id={$row['product_id']}">{$row['product_title']}</a>
                 </h4>
                 <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
-                <a class="btn btn-primary" target="_blank" href="item.php?id={$row['product_id']}">Add to cart</a>
+                <a class="btn btn-primary" target="_blank" href="cart.php?add={$row['product_id']}">Add to cart</a>
             </div>
             
         </div>
