41e5344174427d20e69c1838db4c09c6311ea34a
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-01-31 17:31:26 +0300

    Create cart.php
    linked it to db

---
+++ public/cart.php
@@ -0,0 +1,36 @@
+<?php require_once("../resources/config.php") ?>
+
+
+<?php 
+
+    if(isset($_GET['add'])) {
+
+        $query = query("SELECT * FROM products WHERE product_id=" . escape_string($_GET['add']) . " ");
+
+        confirm($query);
+
+        while($row = fetch_array($query)) {
+
+            if( $row['product_quantity'] != $_SESSION['product_' . $_GET['add']]) {
+
+                $_SESSION['product_' . $_GET['add']] += 1;
+            } else{
+
+                set_message("We only have" . $row['product_quantity'] . " " . "Available");
+                redirect("checkout.php");
+            }
+
+        }
+        
+    }
+
+
+
+
+
+
+
+
+
+
+?>
