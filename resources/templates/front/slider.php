d3f121ba6da338df3df1257d74f42d7d0999dba9
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-01-31 23:39:49 +0300

    referenced the images

--- resources/templates/front/slider.php
+++ resources/templates/front/slider.php
@@ -6,13 +6,13 @@
     </ol>
     <div class="carousel-inner">
         <div class="item active">
-            <img class="slide-image" src="http://placehold.it/800x300" alt="">
+            <img class="slide-image" src="img/shoe5.jpg" alt="">
         </div>
         <div class="item">
-            <img class="slide-image" src="http://placehold.it/800x300" alt="">
+            <img class="slide-image" src="img/shoe4.jpg" alt="">
         </div>
         <div class="item">
-            <img class="slide-image" src="http://placehold.it/800x300" alt="">
+            <img class="slide-image" src="img/shoe3.jpg" alt="">
         </div>
     </div>
     <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
