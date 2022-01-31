d8a7b261db893bcefb0cc396bbf0d273d4ea79c9
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-01-31 18:05:12 +0300

    added two tables to display remove and delete

--- public/checkout.php
+++ public/checkout.php
@@ -2,6 +2,15 @@
 
 <?php include(TEMPLATE_FRONT . DS . "header.php") ?>
 
+<?php 
+
+    if (isset($_SESSION['product_1'])) {
+
+        echo $_SESSION['product_1'];
+    }
+
+?>
+
     <!-- Page Content -->
     <div class="container">
 
@@ -29,6 +38,8 @@
                 <td>$23</td>
                 <td>3</td>
                 <td>2</td>
+                <td><a href="cart.php?remove=1">Remove</a></td>
+                <td><a href="cart.php?delete=1">Delete</a></td>
               
             </tr>
         </tbody>
