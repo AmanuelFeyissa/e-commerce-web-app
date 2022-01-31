c857e3f514925ef27d98ce27d898724b2098add9
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-01-31 16:49:12 +0300

    created a function send_message
    sends message to the provided email address

--- resources/functions.php
+++ resources/functions.php
@@ -190,6 +190,32 @@ function login_user(){
 
 }
 
+function send_message(){
+
+    if(isset($_POST['submit'])){
+        
+        $to         = "amanuelfeyissa45@gmail.com";
+        $from_name  = $_POST['name'];
+        $subject    = $_POST['subject'];
+        $email      = $_POST['email'];
+        $message    = $_POST['message'];
+
+        $headers = "From: {$from_name} {$email}";
+
+        $result = mail($to, $subject, $message, $headers);
+
+        if(!$result) {
+
+            set_message("Sorry we can not send your message");
+            redirect("contact.php");
+        } else {
+
+            set_message("Your message has been sent");
+        }
+    }
+
+}
+
 
 
 /****************************** BACK END FUNCTIONS ************************************/
